e6d726ba61064e0069908b9122d88c73371985a3
João Marcos dos Santos Valente <100044031+progjaoo@users.noreply.github.com>
2022-07-19 20:16:09 -0300

    Add files via upload

---
+++ cep.js
@@ -0,0 +1,29 @@
+function consultaEndereco(){
+    let cep = document.querySelector('#cep').value;
+
+    if(cep.length !== 8) {
+        alert ('CEP inválido!');
+        return;
+    }
+
+    let url = `https://viacep.com.br/ws/${cep}/json/`;
+
+    fetch(url).then(function(response){
+        response.json().then(function(data) {
+            console.log(data)
+            mostrarEndereco(data);  
+        })
+    });
+}
+function mostrarEndereco(dados){
+    let resultado = document.querySelector('#resultado');
+    if(dados.erro){
+        resultado.innerHTML = "Não foi possível localizar endereço!";
+    } else {
+        resultado.innerHTML = `<p>Endereço: ${dados.logradouro}</p>
+                                <p>Bairro: ${dados.bairro}</p>
+                                <p>Cidade: ${dados.localidade} - ${dados.uf}</p>
+                                <p>Siafi: ${dados.siafi}</p>`
+    }
+}
+
